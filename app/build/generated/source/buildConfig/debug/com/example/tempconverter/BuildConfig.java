e5600a5f89bd82cda54efc9581c31fe95f9105f0
Bryant Novas <bryant.novas@gmail.com>
2021-07-25 23:10:33 -0400

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/tempconverter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.tempconverter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.tempconverter";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/example/tempconverter/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.tempconverter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val convertButton: Button = findViewById(R.id.convert)
+        val resetButton: Button = findViewById(R.id.reset)
+        val celciusInput: EditText = findViewById(R.id.celcius)
+        val farenheitInput: EditText = findViewById(R.id.farenheit)
+
+        convertButton.setOnClickListener {
+            val focus = currentFocus?.id.toString()
+            if(focus == "2131231097"){
+               var calc = toFarenheit(celciusInput.text.toString())
+                farenheitInput.setText(calc)
+            }
+            else if(focus == "2131231099"){
+                var calc = toCelcius(farenheitInput.text.toString())
+                celciusInput.setText(calc)
+            }
+        }
+
+        resetButton.setOnClickListener{
+            celciusInput.setText("")
+            farenheitInput.setText("")
+        }
+    }
+
+    private fun toCelcius(txt: String): String {
+        return if(txt != "") {
+            val input = txt.toFloat()
+            val result = (input - 32) * .5556f
+            result.toString()
+        } else {
+            ""
+        }
+    }
+
+    private fun toFarenheit(txt: String): String {
+        return if(txt != "") {
+            val input = txt.toFloat()
+            val result: Float = (input * 1.8f) + 32
+            result.toString()
+        } else {
+            ""
+        }
+    }
+
+
+}
